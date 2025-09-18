438244d0130f0ba41006500bbceeb927e1548c66
Divyesh Khamele <divyesh@signimus.com>
2025-09-19 00:07:49 +0530

    Fix CSS loading issue by importing index.css in main.tsx and App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import './index.css'; // Import the main CSS file
 
 const App: React.FC = () => {
   return (
